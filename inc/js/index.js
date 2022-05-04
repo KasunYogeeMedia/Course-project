66220dc9b3a0be29f017f0817c28334da72f9431
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-05-04 13:33:50 +0530

    form done

--- inc/js/index.js
+++ inc/js/index.js
@@ -1,3 +1,21 @@
-function showform() {
-    document.getElementById('form').style.display = "block";
+function showform(courseid) {
+        document.getElementById('formRegister').style.display = "block";
+}
+
+function openCourse(evt, CourseName) {
+    var i, tabcontent, tablinks;
+
+    $("#courseId").val(CourseName);
+    document.getElementById("courseId").value = CourseName;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(CourseName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
